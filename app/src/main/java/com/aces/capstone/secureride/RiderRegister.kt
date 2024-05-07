c463bd3bf0dad2f5a63a8f63d8aeced0570aa9ed
whilmarbitoco <whlmrbitoco@gmail.com>
2024-05-07 17:31:34 +0800

    second commit

--- app/src/main/java/com/aces/capstone/secureride/RiderRegister.kt
+++ app/src/main/java/com/aces/capstone/secureride/RiderRegister.kt
@@ -3,10 +3,9 @@ package com.aces.capstone.secureride
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
-
+class RiderRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_rider_register)
     }
 }
--- app/src/main/java/com/aces/capstone/secureride/UserRegister.kt
+++ app/src/main/java/com/aces/capstone/secureride/UserRegister.kt
@@ -3,10 +3,9 @@ package com.aces.capstone.secureride
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
-
+class UserRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_user_register)
     }
 }
