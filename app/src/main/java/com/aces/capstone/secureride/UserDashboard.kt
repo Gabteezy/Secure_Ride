97a456b15fb4cecd01d1340f6b4d5100b67f80e1
Gabteezy <gabrielbumaya1@gmail.com>
2024-05-20 09:58:41 +0800

    update

--- app/src/main/java/com/aces/capstone/secureride/UserDashboard.kt
+++ app/src/main/java/com/aces/capstone/secureride/UserDashboard.kt
@@ -2,6 +2,7 @@ package com.aces.capstone.secureride
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 
 class UserDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,24 @@ class UserDashboard : AppCompatActivity() {
         setContentView(R.layout.activity_user_dashboard)
 
 
+        val searchView: SearchView = findViewById(R.id.searchView)
 
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Handle the search query submission
+                query?.let {
+                    // Perform search operation
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle the search query text change
+                newText?.let {
+                    // Update search results in real-time
+                }
+                return false
+            }
+        })
     }
 }
